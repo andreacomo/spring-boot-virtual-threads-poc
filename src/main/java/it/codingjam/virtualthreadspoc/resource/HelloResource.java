c7807d0247c8b3c94ccb15513a5fbc8ee4217d86
Andrea Como <andrea.como@gmail.com>
2025-04-19 13:45:24 +0200

    Parallel api

--- src/main/java/it/codingjam/virtualthreadspoc/resource/HelloResource.java
+++ src/main/java/it/codingjam/virtualthreadspoc/resource/HelloResource.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.*;
+
 @Slf4j
 @RestController
 @RequestMapping("/hello")
@@ -18,5 +20,26 @@ public class HelloResource {
         log.info("Active count in group {}: {}", Thread.currentThread().getThreadGroup().getName(), Thread.currentThread().getThreadGroup().activeCount());
 
         return "Hello World";
+    }
+
+    @GetMapping("/parallel")
+    public String parallelHello() throws ExecutionException, InterruptedException, TimeoutException {
+        try (var executor = Executors.newSingleThreadScheduledExecutor()) {
+            Future<String> hello = executor.submit(() -> getMessage("Hello", 400));
+            Future<String> world = executor.submit(() -> getMessage("World", 600));
+
+            return hello.get(1, TimeUnit.SECONDS) + " " + world.get(1, TimeUnit.SECONDS);
+        }
+    }
+
+    private static String getMessage(String message, long duration) {
+        try {
+            log.info("Before sleep on thread {} for {}", Thread.currentThread(), message);
+            Thread.sleep(duration);
+            log.info("After sleep on thread {} for {}", Thread.currentThread(), message);
+            return message;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
